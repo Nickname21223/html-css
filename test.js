cdb957ef6f8eee8c55308f18594a1f96a2b3d5d5
Nickname21223 <qlsl022772@gmail.com>
2025-05-17 22:26:34 +0900

    6주차

--- test.js
+++ test.js
@@ -1,14 +1,46 @@
 // test.js
-import axios from "axios";
+const http = require("http");
+const url = require("url");
 
-const option = {
-    url: 'https://dummyjson.com/todos/1',
-    method: 'GET',
-    headers: { // ✅ headers가 아니라 header => headers 로 수정해야 함
-        "Content-Type": "application/json"
-    }
+
+
+const home = (req, res) => {
+  res.end("HOME");
 };
 
-axios(option)
-    .then(response => console.log(response.data))
-    .catch(error => console.error(error));
+const user = (req, res) => {
+  const userInfo = url.parse(req.url, true).query;
+  res.end(`[user] name : ${userInfo.name}, age: ${userInfo.age}`);
+};
+
+const feed = (req, res) => {
+  res.end(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+      </ul>
+      `);
+};
+
+const notFound = (req, res) => {
+  res.statusCode = 404;
+  res.end("404 page not found");
+};
+const test = {
+  "/": home,
+  "/user": user,
+  "/feed": feed
+};
+
+http.createServer((req,res) => {
+    const path = url.parse(req.url, true).pathname;
+    res.setHeader("Content-Type","text/html");
+
+  if (path in test) {
+    test[path](req, res);  // 존재하면 해당 경로에 맞는 함수 실행
+  } else {
+    notFound(req, res);  // 경로가 없으면 404 처리
+  }
+
+})
+.listen("3000", () => console.log("라우터"));
